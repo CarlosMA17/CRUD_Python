2e8fe5d55e7b20b1d11ac06ce4f9d04401e47248
Carlos Martel <102035692+CarlosMA17@users.noreply.github.com>
2023-11-02 14:08:42 +0000

    fix: en main el switch acepta los argumentos necesarios y los modulos recogen los modelos enteros

--- Program/main.py
+++ Program/main.py
@@ -9,7 +9,7 @@ vehicles = []
 def switch(case, vehicle, vehicles): 
     match case: 
         case "1": 
-            print(consultVehicle(vehicle))
+            consultVehicle(vehicle)
         case "2": 
             vehicles = addVehicle(vehicles)
         case "3": 
@@ -17,25 +17,29 @@ def switch(case, vehicle, vehicles):
         case "4": 
             vehicles = updateVehicles(vehicles, vehicle)       
         case "5": 
-            vehicles = ["finish"]
+            vehicles = "finish"
 
 print(vehicles)
+
 while exit:
+    vehicle = {}
     option = input("escoja una opcion del programa:\n" +
         "1.- mostrar la base de datos de vehiculos\n" + 
         "2.- añadir un nuevo vehiculo\n" + 
         "3.- eliminar un vehiculo de la base de datos\n" + 
         "4.- actualizar la informacion de un vehiculo\n" +
-        "5.- salir del programa")
+        "5.- salir del programa\n")
     if option == "1" or option == "3" or option == "4":
-        model = input("introduzca el modelo")
-    else :
-        vehicle = {}
-    
-    
+        model = input("introduzca el modelo\n")
+        for vehicleF in vehicles:
+            if model == vehicleF["model"]:
+                vehicle = vehicleF
+
 
     switch(option, vehicle, vehicles)
-    if vehicles == ["finish"]:
-        exit = True
+    if vehicles == []:
+        print("la lista de vehiculos esta vacia\n")
+    elif option == "1": 
+        print("\n")
     else:
-        print(vehicles)
+        print("\nvehiculos: ", vehicles)
