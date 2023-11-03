73ead16880404f7a19c7031fd9d5999faa97ebd0
Carlos Martel <102035692+CarlosMA17@users.noreply.github.com>
2023-11-03 08:37:30 +0000

    Se mejoro la indentacion del codigo

--- Program/main.py
+++ Program/main.py
@@ -7,6 +7,7 @@ from modules.updateVehicles import updateVehicles
  The variables "exit" and  "vahicles" are created,
 one to control the loop and another to save all the vehicles.
 '''
+
 exit = True  
 vehicles = [] 
 
@@ -14,23 +15,29 @@ vehicles = []
 The function is defined with the switch and passing the variables to modify
 '''
 
-def switch(case, vehicle, vehicles):                   
+def switch(case, vehicle, vehicles):  
+                     
     match case:                                         
         case "1": 
             for vehicle in vehicles:
                 consultVehicle(vehicle)
             return True
+        
         case "2": 
             vehicles = addVehicle(vehicles)
             return True
+        
         case "3": 
             vehicles = deleteVehicle(vehicles, vehicle)
             return True
+        
         case "4": 
             vehicles = updateVehicles(vehicles, vehicle) 
-            return True      
+            return True 
+             
         case "5": 
             return False
+        
         case _:
             print("no es valido")
             
@@ -42,17 +49,21 @@ the user is also asked.
 
 
 while exit:
+    
     vehicle = {}
+    
     option = input("escoja una opcion del programa:\n" +
         "1.- mostrar la base de datos de vehiculos\n" + 
         "2.- añadir un nuevo vehiculo\n" + 
         "3.- eliminar un vehiculo de la base de datos\n" + 
         "4.- actualizar la informacion de un vehiculo\n" +
         "5.- salir del programa\n")
+    
     if option == "3" or option == "4":
         brand = input("introduzca la marca\n")
         model = input("introduzca el modelo\n")
         year = input("introduzca el año\n")
+        
         for vehicleF in vehicles:
             if brand == vehicleF["brand"] and model == vehicleF["model"] and year == vehicleF["year"]:
                     vehicle = vehicleF
@@ -63,6 +74,7 @@ while exit:
     '''
     
     exit = switch(option, vehicle, vehicles)
+    
     if exit == True:
         if vehicles == []:
             print("la lista de vehiculos esta vacia\n")
